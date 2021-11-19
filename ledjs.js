ca2042a3724bd15ba9e6affd8abf540584abff7d
Paloma Vilchis <paloma.vilchis@gmail.com>
2021-11-18 18:15:50 -0600

    Create ledjs.js

---
+++ ledjs.js
@@ -0,0 +1,16 @@
+
+var Gpio = require(‘onoff’).Gpio; 
+var led = new Gpio(17, ‘out’);     
+
+var button = new Gpio(27, ‘in’, ‘rising’, {debounceTimeout: 10});
+var estado = 0;
+
+button.watch((err, value) => {
+ if (err) {
+   throw err;
+ }
+ estado = !estado;
+ if(estado) { led.writeSync(1); }
+ else {led.writeSync(0); }
+
+});
